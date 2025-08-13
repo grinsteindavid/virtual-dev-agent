98e1a95ebe41955063d619aed62fcc393ca21337
David Miranda Grinstein <davidmirandagrinstein@gmail.com>
2025-08-13 18:21:51 -0400

    chore: migrate Jest configuration from ESM to CommonJS with Babel support

--- mcps/jira/jest.config.cjs
+++ mcps/jira/jest.config.cjs
@@ -1,17 +1,14 @@
 /** @type {import('jest').Config} */
-export default {
+module.exports = {
   // Use node environment
   testEnvironment: 'node',
   
-  // Transform ESM modules
-  transform: {},
-  
-  // Modify how Jest resolves modules
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1'
+  // Transform ESM modules to CommonJS for testing
+  transform: {
+    '^.+\\.js$': 'babel-jest'
   },
   
-  // Configure Jest to handle ESM properly
+  // Configure Jest to handle modules properly
   transformIgnorePatterns: [
     'node_modules/(?!(jira-client|dotenv|winston)/)'
   ],
